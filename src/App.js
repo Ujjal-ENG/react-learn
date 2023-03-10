41a1898c7e6ffc0a5248ac1cbdb5d244f73bdae3
user <ujjalroy7862@gmail.com>
2023-03-10 20:40:15 +0600

    set the item in the localstorage using useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import AddCard from './components/AddCard';
 import ContactList from './components/ContactList';
@@ -6,11 +7,21 @@ import ContactList from './components/ContactList';
 import Header from './components/Header';
 
 const App = () => {
+    const LOACAL_STORAGE_KEY = 'contacts';
     const [contacts, setContacts] = useState([]);
 
     const addContactHandler = (contact) => {
         setContacts([...contacts, contact]);
     };
+
+    useEffect(() => {
+        const getItem = JSON.parse(localStorage.getItem(LOACAL_STORAGE_KEY));
+        if (getItem) setContacts(getItem);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem(LOACAL_STORAGE_KEY, JSON.stringify(contacts));
+    }, [contacts]);
 
     return (
         <>
